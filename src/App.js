5de25a8a6228056dc68f12baf59905951a4eea6a
hansonliao98 <109989314+hansonliao98@users.noreply.github.com>
2023-08-25 16:51:35 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./Components/About/About";
+import BannerIntro from "./Components/BannerIntro/BannerIntro";
+import Navbar from "./Components/Navbar/Navbar";
+import Skills from "./Components/Skills/Skills";
+import Partfolio from "./Components/Portfolio/Partfolio";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
+import { useEffect, useState } from "react";
+import Popup from "./Components/Portfolio/Popup";
+import { userContext } from "./Context/userContext";
+import Design from "./Components/Design/Design";
 
 function App() {
+  const [isVideo, setIsVideo] = useState(false);
+  const [URL, setURL] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const loadVideoHandler = (url) => {
+    setIsVideo(!isVideo);
+    setURL(url);
+    console.log(URL);
+  };
+
+  const isLoadingHandler = () => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    isLoadingHandler();
+
+    console.log(isLoading);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`text-white`}>
+      <userContext.Provider
+        value={{ isVideo, setIsVideo, loadVideoHandler, URL }}
+      >
+        <Popup isVideo={isVideo} setIsVideo={setIsVideo} URL={URL}>
+          <h3>My popup</h3>
+        </Popup>
+        <Navbar className="" />
+        <BannerIntro />
+        <div className={`${isLoading && "hidden"}`}>
+          <div className="bg-image2 bg-cover -mt-1 scroll-smooth">
+            <About />
+            <Skills />
+            <Partfolio loadVideoHandler={loadVideoHandler} />
+            <Design />
+          </div>
+          <Footer />
+        </div>
+      </userContext.Provider>
     </div>
   );
 }
